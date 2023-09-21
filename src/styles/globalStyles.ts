e28b479159327384406ba30bbdc3d1568d9d1462
MaysharaF <mayshara@alunos.utfpr.edu.br>
2023-09-20 23:33:50 -0300

    feat: inicializando aplicação com estruturação padrão

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  :root {
+    --white: #FFFFFF;
+    --light-grey: #969FAD;
+    --medium-gray: #7C8798;
+    --dark-blue: #262E38;
+    --very-dark-blue: #131518;
+    --black-gradient: radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)
+
+    --orange: #FC7614;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box
+  }
+
+   body {
+    width: 100%;
+    height: 100vh;
+    background-color: var(--very-dark-blue);
+  }
+
+  body, input, button {
+    font: 15px Overpass, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
